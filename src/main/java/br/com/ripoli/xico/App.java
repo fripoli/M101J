05594f9e52591323b529130e9f2c263e2095d2d6
Francisco Ripoli <xico@ripoli.com.br>
2014-07-27 00:17:06 +0100

    innitial commit

---
+++ src/main/java/br/com/ripoli/xico/App.java
@@ -0,0 +1,13 @@
+package br.com.ripoli.xico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
