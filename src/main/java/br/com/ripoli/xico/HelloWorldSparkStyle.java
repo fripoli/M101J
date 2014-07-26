b7cfd8e82c4a9c8dd3c23cb3f0a4f867e6637954
Francisco Ripoli <xico@ripoli.com.br>
2014-07-27 00:39:57 +0100

    hello world spark style

---
+++ src/main/java/br/com/ripoli/xico/HelloWorldSparkStyle.java
@@ -0,0 +1,18 @@
+package br.com.ripoli.xico;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+public class HelloWorldSparkStyle {
+
+	public static void main(String[] args) {
+		Spark.get(new Route("/") {
+			@Override
+			public Object handle(Request request, Response response) {
+				return "Hello World from spark";
+			}
+		});
+	}
+}
