2d99e9260c698c141cc988130559c8ed36416486
Francisco Ripoli <xico@ripoli.com.br>
2014-07-27 00:20:52 +0100

    code formatting

--- src/main/java/br/com/ripoli/xico/App.java
+++ src/main/java/br/com/ripoli/xico/App.java
@@ -2,12 +2,9 @@ package br.com.ripoli.xico;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
