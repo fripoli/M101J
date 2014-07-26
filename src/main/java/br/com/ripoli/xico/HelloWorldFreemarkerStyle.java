7f80c161d93de9ec248501031d5fd452c504df7b
Francisco Ripoli <xico@ripoli.com.br>
2014-07-27 00:48:11 +0100

    hello world freemarker style

---
+++ src/main/java/br/com/ripoli/xico/HelloWorldFreemarkerStyle.java
@@ -0,0 +1,29 @@
+package br.com.ripoli.xico;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorldFreemarkerStyle {
+
+	public static void main(String[] args) throws IOException, TemplateException {
+		Configuration configuration = new Configuration();
+
+		configuration.setClassForTemplateLoading(HelloWorldFreemarkerStyle.class, "/");
+
+		Template helloTemplate = configuration.getTemplate("hello.ftl");
+		StringWriter writer = new StringWriter();
+
+		Map<String, Object> helloMap = new HashMap<String, Object>();
+		helloMap.put("name", "Freemarker");
+
+		helloTemplate.process(helloMap, writer);
+
+		System.out.println(writer);
+	}
+}
